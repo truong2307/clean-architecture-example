ed247eb0ba1501eb102f64de200540abee4bf204
Nguyen Van Truong <toiiuvn012@gmail.com>
2023-06-17 18:10:09 +0700

    Domain modìy: Add Entities: - Quiz Classroom - Student in classroom

---
+++ QuizApp.Domain/Entities/QuizClassRoom.cs
@@ -0,0 +1,14 @@
+﻿namespace QuizApp.Domain.Entities
+{
+    public class QuizClassRoom
+    {
+        private int _quizId;
+        private int _classRoomId;
+
+        public QuizClassRoom(int quizId, int classRoomId)
+        {
+            _quizId = quizId;
+            _classRoomId = classRoomId;
+        }
+    }
+}
---
+++ QuizApp.Domain/Entities/StudentInClassRoom.cs
@@ -0,0 +1,14 @@
+﻿namespace QuizApp.Domain.Entities
+{
+    public class StudentInClassRoom
+    {
+        private string _studentId;
+        private int _classRoomId;
+
+        public StudentInClassRoom(string studentId, int classRoomId)
+        {
+            _studentId = studentId;
+            _classRoomId = classRoomId;
+        }
+    }
+}
