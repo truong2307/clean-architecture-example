e4cbefce743eee3bda03c17d9e5b112fc042a133
Nguyen Van Truong <toiiuvn012@gmail.com>
2023-06-20 22:46:14 +0700

    Web api add: 1. Program: add infrastructure service 2. Appsetting: add connection string 3. add package EntityFrameworkCore.Design

--- QuizApp.API/Program.cs
+++ QuizApp.API/Program.cs
@@ -1,6 +1,9 @@
+using QuizApp.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
